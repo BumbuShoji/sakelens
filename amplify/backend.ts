1db7066a3ff2f90ac2b3dd9acd6964fa99c190b3
Bumbu Shoji <82212063+BumbuShoji@users.noreply.github.com>
2025-03-21 00:43:40 +0900

    fix amplify config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
-import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { defineBackend } from '@aws-amplify/backend';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -8,4 +8,6 @@ import { data } from './data/resource';
 defineBackend({
   auth,
   data,
+  // シークレットを使用する場合はSSMシークレット設定に環境変数名を指定
+  // secretsReferences: ['GEMINI_API_KEY', 'DEEPSEEK_API_KEY']
 });
